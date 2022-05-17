294f479fb937a962cf39c4be88758fd2099e05dc
Avinash Upadhyaya <avinashupadhya99@gmail.com>
2022-05-17 12:33:07 +0530

    fix: include monitor type in update-monitor-title

--- update-monitor-title.py
+++ update-monitor-title.py
@@ -9,9 +9,9 @@ from datadog_api_client.v1.api.monitors_api import MonitorsApi
 configuration = Configuration()
 with ApiClient(configuration) as api_client:
     api_instance = MonitorsApi(api_client)
-    response = api_instance.list_monitors(monitor_tags="env:sit,component:abc")
+    response = api_instance.search_monitors(query="type:metric tag:(\"env:sit\" AND \"component:abc\")")
 
-    for monitor in response:
+    for monitor in response['monitors']:
         new_name = ''
         if "APM" in monitor.name: 
             continue
