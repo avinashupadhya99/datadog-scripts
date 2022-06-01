64ee58a397db7833afc62752d7936527ba3c7ef3
Avinash Upadhyaya <avinashupadhya99@gmail.com>
2022-06-01 20:15:53 +0530

    feat: format the monitor changes report

--- monitor_changes.py
+++ monitor_changes.py
@@ -7,10 +7,20 @@ from datadog_api_client.v1.api.events_api import EventsApi
 import datetime
 from mistletoe import Document, HTMLRenderer
 
-now = datetime.datetime.now()
-yesterday = now - datetime.timedelta(days=2)
+def get_background_color(event_string):
+    background_color='white'
+    if "created" in event_string:
+        background_color = '#E3FCBF'
+    elif "deleted" in event_string:
+        background_color = '#FFC3C3'
+    elif "modified" in event_string:
+        background_color = '#FEF9A7'
+    return background_color
 
-output_file_name = "output-{}.html".format(now.strftime("%m-%d-%Y_%H-%M-%S"))
+now = datetime.datetime.now()
+yesterday = now - datetime.timedelta(days=1)
+
+output_file_name = "monitor-{}.html".format(now.strftime("%m-%d-%Y_%H-%M-%S"))
 
 configuration = Configuration()
 with ApiClient(configuration) as api_client:
@@ -18,22 +28,30 @@ with ApiClient(configuration) as api_client:
     response = api_instance.list_events(
         start=int(yesterday.timestamp()),
         end=int(now.timestamp()),
-        tags="env:*prod*,monitor,source:datadog"
+        tags="env:(prod* OR ne-prod*),monitor,source:datadog"
     )
+    print(response.events)
+    f = open(output_file_name, "w")
+    f.write("<center><h1>Production Datadog monitor modifications report</h1><p>The report contains the modifications for all monitor in production for the past 1 day including the creation and deletion of monitors.</p></center>")
+    f.write("<h3>Summary</h3><ul><li>Date and time of the report generation - <b>{}</b></li><li>There are <b>{}</b> monitor(s) modified/created/deleted in production in the past 24 hours.</li></ul>".format(now.strftime("%m-%d-%Y %H:%M:%S"), len(response['events'])))
+    f.close()
 
     if len(response['events']) == 0:
         print("No monitors were modified in the past 24 hours")
-        f = open(output_file_name, "a")
-        f.write("<h1>No monitors in the production environment were modified in the past 24 hours</h1>")
-        f.close()
+
 
     for event in response['events']:
         with HTMLRenderer() as renderer:
             event_string = event.text.replace("%%%", "") # Remove %%% from the event string
+            background_color = get_background_color(event_string)
             ast_doc = Document(event_string)     # parse the lines into AST
             rendered = renderer.render(ast_doc)
-            f = open(output_file_name, "a")
             formatted_rendered = rendered.replace("href=\"/monitors", "target=\"_blank\" href=\"https://app.datadoghq.com/monitors")
             formatted_rendered += "\n<hr>"
+            event_date_time = datetime.datetime.fromtimestamp(event.date_happened)
+            f = open(output_file_name, "a")
+            f.write("<div style=\"background-color:{}\">".format(background_color))
+            f.write("Date and time - {}\n".format(event_date_time))
             f.write(formatted_rendered)
+            f.write('</div>')
             f.close()
